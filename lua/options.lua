6ade943540877c41802377581000e4e619b12e42
minh25 <minh00012258@gmail.com>
2024-03-21 18:44:22 +0700

    update

--- lua/options.lua
+++ lua/options.lua
@@ -5,5 +5,29 @@ require "nvchad.options"
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
-vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"
+local vim = vim
+local opt = vim.opt
+opt.foldmethod = "expr"
+opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+function _G.MyFoldText()
+  return vim.fn.getline(vim.v.foldstart) .. " ... " .. vim.fn.getline(vim.v.foldend):gsub("^%s*", "")
+end
+function _G.MyFoldText2()
+  local foldstart = vim.v.foldstart
+  local foldend = vim.v.foldend
+  -- local foldlevel = vim.v.foldlevel
+  local foldtext = vim.fn.getline(foldstart)
+
+  -- Calculate the number of hidden lines
+  local hiddenLines = foldend - foldstart
+
+  -- Format the fold text
+  foldtext = foldtext .. " +" .. hiddenLines .. " lines"
+
+  -- Indent the fold text based on the fold level
+  -- foldtext = string.rep(' ', foldlevel) .. foldtext
+
+  return foldtext
+end
+vim.opt.foldtext = "v:lua.MyFoldText2()"
